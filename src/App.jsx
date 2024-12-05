4f7ff158e5836bab83fe8ba67319d36f6c4b3a57
Austin <christopheraustinwallace@gmail.com>
2024-12-05 10:21:04 -0500

    setup an example css module
    Set up an example css module for quick usage of this template

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Link } from "react-router-dom";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 // import "./App.css";
+import Button from "./components/Button/Button";
 
 const App = () => {
   return (
     <div>
       <h1>Hello from the main page of the app!</h1>
       <p>Here are some examples of links to other pages</p>
+      <Button></Button>
       <nav>
         <ul>
           <li>
